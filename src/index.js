eb3a77f865749c983b6cc19f99a5c3aac9a173fd
andrearangel45 <124049331@upq.edu.mx>
2026-02-11 22:31:10 -0600

    Rutas
    Se agregaron las rutas de productos y categoria

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { route } = require('./routes/productoRoute');
+const router = require('./routes/productoRoute');
+const categoriaRoute = require('./routes/categoriaRoute');
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/productos', route);
+app.use('/api/productos', router);
+app.use('/api/categoria', categoriaRoute);
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log("Servicio arriba"));
