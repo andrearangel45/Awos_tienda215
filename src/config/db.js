48fe95913fa827a87f9c2e44896e80c9c0826a05
Andrea Sánchez Rangel <124049331@upq.edu.mx>
2026-04-01 18:46:10 -0600

    Refactor database connection to use DATABASE_URL cambio db

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on('connect', () => console.log('Conexion exitosa'));
